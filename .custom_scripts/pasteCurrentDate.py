959c6cdf7ee11944d33ebea39d6a8c18beac3c8e
Kinille <nickchoffman@gmail.com>
2020-04-02 03:27:06 -0600

    Rename date script

--- .custom_scripts/pasteCurrentDate.py
+++ .custom_scripts/pasteCurrentDate.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
-import pyperclip
+from pyperclip import copy, paste
 from datetime import date
+import time
 
 today = date.today()
 
 dateString = today.strftime("%F %A")
-pyperclip.copy(dateString)
+buffer = paste()
+
+copy(dateString)
+time.sleep(5)
+copy(buffer)
